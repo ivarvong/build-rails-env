6381460ed908d7a8de80b77f4e765513a95ab5a5
Ivar Vong <ivar@ivarvong.com>
2012-12-20 19:27:48 -0800

    Create as_deployer.sh

---
+++ as_deployer.sh
@@ -0,0 +1,21 @@
+echo "installing rbenv..."
+
+curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+
+curl -O https://raw.github.com/ivarvong/build-rails-env/master/dotbashrc 
+echo dotbashrc >> ~/.bashrc
+
+source ~/.bashrc
+
+rbenv bootstrap-ubuntu-12-04
+rbenv install 1.9.3-p125
+
+rbenv global 1.9.3-p125
+ruby -v
+
+gem install bundler --no-ri --no-rdoc
+rbenv rehash
+bundle -v
+
+echo "ssh to github..."
+ssh git@github.com
