1838f01553e28f7685d99c8c75efcf0a4734d45d
Ivar Vong <ivar@ivarvong.com>
2012-12-21 22:20:02 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 apt-get update
+apt-get -y install curl git-core python-software-properties
 
 add-apt-repository ppa:nginx/stable
 add-apt-repository ppa:pitti/postgresql
@@ -8,8 +9,10 @@ apt-get update
 
 apt-get -y install postgresql libpq-dev
 apt-get -y install nodejs
-
 apt-get -y install nginx
+apt-get -y install memcached
+apt-get -y install redis-server
+
 service nginx start
 
 adduser deployer --ingroup sudo
